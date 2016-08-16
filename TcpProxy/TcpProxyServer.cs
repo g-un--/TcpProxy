2be8f318fcf9b22ad25aab84b5146caa9699a138
g-un-- <office.gino@gmail.com>
2016-08-16 12:07:56 +0300

    Use akka.net logging

--- TcpProxy/TcpProxyServer.cs
+++ TcpProxy/TcpProxyServer.cs
@@ -1,4 +1,5 @@
 ﻿using Akka.Actor;
+using Akka.Event;
 using Akka.IO;
 using System;
 using System.Net;
@@ -7,16 +8,17 @@ namespace TcpProxy
 {
     public class TcpProxyServer : ReceiveActor
     {
+        private readonly ILoggingAdapter _log = Logging.GetLogger(Context);
         private readonly IActorRef manager = Context.System.Tcp();
 
         public TcpProxyServer(EndPoint listeningEndPoint, EndPoint linkEndPoint)
         {
             manager.Tell(new Tcp.Bind(Self, listeningEndPoint));
 
-            Receive<Tcp.Bound>(message => Console.WriteLine($"Listening on: {message.LocalAddress}"));
+            Receive<Tcp.Bound>(message => _log.Debug($"Proxy listening on: {message.LocalAddress}"));
             Receive<Tcp.Connected>(message =>
             {
-                Console.WriteLine($"Client connected: {message.RemoteAddress}");
+                _log.Debug($"Client connected: {message.RemoteAddress}");
                 var linkActor = Context.ActorOf(Props.Create(() => new TcpLink(message.RemoteAddress, Sender, linkEndPoint)));
                 Sender.Tell(new Tcp.Register(linkActor));
             });
